8a41231e72becf731758f69f4dd700061c838cac
Lee Herbst <admin@Lees-iMac.local>
2017-09-11 16:17:01 -0400

    Next step done

--- Snippit/Snippit/TextSnippitEntryViewController.swift
+++ Snippit/Snippit/TextSnippitEntryViewController.swift
@@ -17,12 +17,27 @@ class TextSnippitEntryViewController: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
       super.viewDidLoad()
         textView.delegate = self
+        textView.inputAccessoryView = createKeyboardToolbar()
         textView.becomeFirstResponder()
     }
 
 
     //MARK: UITextViewDelegate
     func textViewDidEndEditing(_ textView: UITextView){}
+    
+    
+    func createKeyboardToolbar() -> UIView {
+        let keyboardToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonPressed))
+        keyboardToolbar.setItems([flexSpace, doneButton], animated: false)
+        
+        return keyboardToolbar
+    }
+    
+    func doneButtonPressed(){
+        textView.resignFirstResponder
+    }
 
 }
 
